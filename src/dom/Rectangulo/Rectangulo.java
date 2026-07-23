cdedfe391be76d3785a9c6ab70f4598944d1cc61
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-23 14:55:27 -0500

    Archvios Base

---
+++ src/dom/Rectangulo/Rectangulo.java
@@ -0,0 +1,5 @@
+package dom.Rectangulo;
+
+public class Rectangulo {
+
+}
---
+++ src/dom/Rectangulo/TestRectangulo.java
@@ -0,0 +1,10 @@
+package dom.Rectangulo;
+
+public class TestRectangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
