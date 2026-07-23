4861c0867f3cf180c9fb514956d0523f2ad806d7
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-23 15:03:26 -0500

    Agregamos la funcion Area 1

--- src/dom/Rectangulo/Rectangulo.java
+++ src/dom/Rectangulo/Rectangulo.java
@@ -2,4 +2,12 @@ package dom.Rectangulo;
 
 public class Rectangulo {
 
+	public int base;
+	public int altura;
+
+	public int area() {
+		int area = base * altura;
+		return area;
+	}
+
 }
--- src/dom/Rectangulo/TestRectangulo.java
+++ src/dom/Rectangulo/TestRectangulo.java
@@ -4,7 +4,11 @@ public class TestRectangulo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Rectangulo rec = new Rectangulo();
+		rec.altura = 3;
+		rec.base = 4;
+		int resultadoArea = rec.area();
+		System.out.println("El area es: " + resultadoArea);
 	}
 
 }
