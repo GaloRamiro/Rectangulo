a2fabd90962faa8ea56332c460a8a9dc80b66cb1
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-23 15:04:42 -0500

    Aggregar funcion rectangulo 2 del area

--- src/dom/Rectangulo/TestRectangulo.java
+++ src/dom/Rectangulo/TestRectangulo.java
@@ -9,6 +9,15 @@ public class TestRectangulo {
 		rec.base = 4;
 		int resultadoArea = rec.area();
 		System.out.println("El area es: " + resultadoArea);
+		
+		
+		
+		
+		Rectangulo rec2 = new Rectangulo();
+		rec2.altura = 6;
+		rec2.base = 67;
+		int resultadoArea2 = rec2.area();
+		System.out.println("El area es: " + resultadoArea2);
 	}
 
 }
