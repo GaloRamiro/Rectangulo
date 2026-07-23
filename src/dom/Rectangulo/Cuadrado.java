10f30b1fd9b64a975e14eff07e04eef9c7254e9d
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-23 15:25:09 -0500

    Agregarmos area y perimetro de un cuadrado

---
+++ src/dom/Rectangulo/Cuadrado.java
@@ -0,0 +1,16 @@
+package dom.Rectangulo;
+
+public class Cuadrado {
+	public int lado;
+
+	public double calcularArea() {
+		double areaCuadrado = Math.pow(lado, 2);
+		return areaCuadrado;
+	}
+
+	public double calcularPerimetro() {
+		double perimetroCuadrado = lado * 4;
+		return perimetroCuadrado;
+	}
+
+}
---
+++ src/dom/Rectangulo/TestCuadrado.java
@@ -0,0 +1,35 @@
+package dom.Rectangulo;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Cuadrado c1 = new Cuadrado();
+	        c1.lado = 3;
+
+	        Cuadrado c2 = new Cuadrado();
+	        c2.lado = 5;
+
+	        Cuadrado c3 = new Cuadrado();
+	        c3.lado = 8;
+
+	        System.out.println("Cuadrado 1");
+	        System.out.println("Área: " + c1.calcularArea());
+	        System.out.println("Perímetro: " + c1.calcularPerimetro());
+
+	        System.out.println();
+
+	        System.out.println("Cuadrado 2");
+	        System.out.println("Área: " + c2.calcularArea());
+	        System.out.println("Perímetro: " + c2.calcularPerimetro());
+
+	        System.out.println();
+
+	        System.out.println("Cuadrado 3");
+	        System.out.println("Área: " + c3.calcularArea());
+	        System.out.println("Perímetro: " + c3.calcularPerimetro());
+
+
+	}
+
+}
